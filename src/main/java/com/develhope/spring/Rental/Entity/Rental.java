b56ae75b0818200c5e98b8ab0080512b137f8555
saverio <saveriodinapoli99@gmail.com>
2024-02-29 15:42:35 +0100

    commit Rental Class

---
+++ src/main/java/com/develhope/spring/Rental/Entity/Rental.java
@@ -0,0 +1,114 @@
+package com.develhope.spring.Rental.Entity;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Table
+@Entity
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private LocalDate startDate;
+    @Column
+    private LocalDate endDate;
+    @Column
+    private double dailyCost;
+    @Column
+    private double totalCost;
+    @Column(nullable = false)
+    private boolean paidFlag;
+    //@OneToMany
+    //private Veicolo veicolo;
+    @Enumerated(EnumType.STRING)
+    private RentalStatus rentalStatus;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getDailyCost() {
+        return dailyCost;
+    }
+
+    public void setDailyCost(double dailyCost) {
+        this.dailyCost = dailyCost;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public boolean isPaidFlag() {
+        return paidFlag;
+    }
+
+    public void setPaidFlag(boolean paidFlag) {
+        this.paidFlag = paidFlag;
+    }
+
+
+    public RentalStatus getRentalStatus() {
+        return rentalStatus;
+    }
+
+    public void setRentalStatus(RentalStatus rentalStatus) {
+        this.rentalStatus = rentalStatus;
+    }
+
+    public Rental(Long id, LocalDate startDate, LocalDate endDate, double dailyCost, double totalCost, boolean paidFlag, RentalStatus rentalStatus) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.dailyCost = dailyCost;
+        this.totalCost = totalCost;
+        this.paidFlag = paidFlag;
+        //this.veicolo = veicolo;
+        this.rentalStatus = rentalStatus;
+
+    }
+
+    public Rental() {
+    }
+
+    @Override
+    public String toString() {
+        return "Rental{" +
+                "id=" + id +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", dailyCost=" + dailyCost +
+                ", totalCost=" + totalCost +
+                ", paidFlag=" + paidFlag +
+                ", rentalStatus=" + rentalStatus +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/develhope/spring/Rental/RentalService/RentalService.java
@@ -0,0 +1,4 @@
+package com.develhope.spring.Rental.RentalService;
+
+public class RentalService {
+}
