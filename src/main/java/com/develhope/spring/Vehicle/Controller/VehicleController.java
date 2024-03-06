d6cb08f4e77515c8740fb7fbda8717544bf83175
udhin98 <udhin1998@gmail.com>
2024-03-06 10:30:14 +0100

    autowired

--- src/main/java/com/develhope/spring/Vehicle/Controller/VehicleController.java
+++ src/main/java/com/develhope/spring/Vehicle/Controller/VehicleController.java
@@ -1,9 +1,13 @@
 package com.develhope.spring.Vehicle.Controller;
 
+import com.develhope.spring.Vehicle.Service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class VehicleController {
+    @Autowired
+    VehicleService vehicleService;
 }
--- src/main/java/com/develhope/spring/Vehicle/Service/VehicleService.java
+++ src/main/java/com/develhope/spring/Vehicle/Service/VehicleService.java
@@ -1,7 +1,11 @@
 package com.develhope.spring.Vehicle.Service;
 
+import com.develhope.spring.Vehicle.Repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleService {
+    @Autowired
+    VehicleRepository vehicleRepository;
 }
