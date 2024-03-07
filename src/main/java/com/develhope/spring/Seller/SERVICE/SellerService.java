e2b8a8af83618dff7d2e32c193aa5ed57b0c139f
Margherita <margherita.deodato14@gmail.com>
2024-03-07 10:56:56 +0100

    commit metodi

--- src/main/java/com/develhope/spring/Seller/SERVICE/SellerService.java
+++ src/main/java/com/develhope/spring/Seller/SERVICE/SellerService.java
@@ -1,11 +1,55 @@
 package com.develhope.spring.Seller.SERVICE;
 
+import com.develhope.spring.Order.Entity.Order;
+import com.develhope.spring.Order.Entity.OrderStatus;
+import com.develhope.spring.Order.Repository.OrderRepository;
+import com.develhope.spring.Seller.ENTITY.SellerEntity;
 import com.develhope.spring.Seller.REPOSITORY.SellerRepository;
+import com.develhope.spring.Vehicle.Entity.Vehicle;
+import com.develhope.spring.Vehicle.Repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class SellerService {
     @Autowired
     SellerRepository sellerRepository;
+
+    @Autowired
+    VehicleRepository vehicleRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public Vehicle getVehicleById(Long vehicleId) { //lavorando su veicolo prendo quello
+        Optional<Vehicle> vehicleEntityOptional = vehicleRepository.findById(vehicleId);
+        if (vehicleEntityOptional.isPresent()) {
+            Vehicle vehicleEntity = vehicleEntityOptional.get();
+            return vehicleEntity;
+        } else {
+            return null;
+        }
+    }
+
+    public Order newOrderVehicle(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public void deleteOrder(Long orderId) { //nome id che c'è in ORDER
+        orderRepository.deleteById(orderId);
+    }
+
+    public Order updateOrder(Order updateOrder, Long orderId) {
+        Order newUpdateOrder = orderRepository.findById(orderId).orElse(null);
+        if (newUpdateOrder != null) {
+
+        }
+
+    }
 }
+
