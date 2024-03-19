205f5c998913ea7056a8865070e51985da6216e9
saverio <saveriodinapoli99@gmail.com>
2024-03-19 11:19:49 +0100

    commit

--- src/main/java/com/develhope/spring/Purchase/Entity/Purchase.java
+++ src/main/java/com/develhope/spring/Purchase/Entity/Purchase.java
@@ -17,7 +17,7 @@ public class Purchase {
     private Boolean isPaid;
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private PurchaseStatus orderStatus;
+    private PurchaseStatus purchaseStatus;
     @OneToOne
     @JoinColumn(name = "purchaseId", referencedColumnName = "vehicleId")
     private Vehicle vehicle;
@@ -49,12 +49,12 @@ public class Purchase {
         isPaid = paid;
     }
 
-    public PurchaseStatus getOrderStatus() {
-        return orderStatus;
+    public PurchaseStatus getPurchaseStatus() {
+        return purchaseStatus;
     }
 
-    public void setOrderStatus(PurchaseStatus orderStatus) {
-        this.orderStatus = orderStatus;
+    public void setPurchaseStatus(PurchaseStatus purchaseStatus) {
+        this.purchaseStatus = purchaseStatus;
     }
 
     public Vehicle getVehicle() {
@@ -73,11 +73,11 @@ public class Purchase {
         this.buyer = buyer;
     }
 
-    public Purchase(Long purchaseId, double advance, Boolean isPaid, PurchaseStatus orderStatus, Vehicle vehicle, Buyer buyer) {
+    public Purchase(Long purchaseId, double advance, Boolean isPaid, PurchaseStatus purchaseStatus, Vehicle vehicle, Buyer buyer) {
         this.purchaseId = purchaseId;
         this.advance = advance;
         this.isPaid = isPaid;
-        this.orderStatus = orderStatus;
+        this.purchaseStatus = purchaseStatus;
         this.vehicle = vehicle;
         this.buyer = buyer;
     }
@@ -91,7 +91,7 @@ public class Purchase {
                 "purchaseId=" + purchaseId +
                 ", advance=" + advance +
                 ", isPaid=" + isPaid +
-                ", orderStatus=" + orderStatus +
+                ", purchaseStatus=" + purchaseStatus +
                 ", vehicle=" + vehicle +
                 ", buyer=" + buyer +
                 '}';
--- src/main/java/com/develhope/spring/Purchase/Service/PurchaseService.java
+++ src/main/java/com/develhope/spring/Purchase/Service/PurchaseService.java
@@ -1,12 +1,59 @@
 package com.develhope.spring.Purchase.Service;
 
+
 import com.develhope.spring.Purchase.Entity.Purchase;
 import com.develhope.spring.Purchase.Repository.PurchaseRepository;
+import com.develhope.spring.Vehicle.Entity.VehicleStatus;
+import com.develhope.spring.Vehicle.Repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PurchaseService {
     @Autowired
     PurchaseRepository purchaseRepository;
+    @Autowired
+    VehicleRepository vehicleRepository;
+
+    public List<Purchase> purchaseList(Purchase purchase) {
+        return purchaseRepository.findAll();
+    }
+
+    public Purchase purchasableOrder(Purchase purchase) {
+        if (purchase.getVehicle() != null && purchase.getVehicle().getVehicleStatus() == VehicleStatus.PURCHASABLE) {
+            purchaseRepository.save(purchase);
+            return purchase;
+        }
+        return null;
+    }
+
+    public void deletePurchase(Long purchaseId) {
+        purchaseRepository.deleteById(purchaseId);
+    }
+
+    public Purchase createPurchase(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
+
+    public Purchase updatePurchase(Long purchaseId, Purchase purchase) {
+        Purchase savePurchase = purchaseRepository.findById(purchaseId).orElse(null);
+        if (savePurchase != null) {
+            savePurchase.setPaid(purchase.getPaid());
+            savePurchase.setAdvance(purchase.getAdvance());
+            savePurchase.setPurchaseStatus(purchase.getPurchaseStatus());
+            return purchaseRepository.save(savePurchase);
+        }
+        return null;
+    }
+
+    public Purchase setPurchaseStatus(Long purchaseId, Boolean isPaid) {
+        Optional<Purchase> student = purchaseRepository.findById(purchaseId);
+        if (student.isPresent()) {
+            student.get().setPaid(isPaid);
+            return purchaseRepository.saveAndFlush(student.get());
+        } else return null;
+    }
 }
